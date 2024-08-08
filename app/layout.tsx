4c58c0fd045851eabb57192763da01f368e21a90
Juan Saldarriaga <juan@ocmiwc.com>
2024-08-08 16:18:10 -0500

    feat: vercel analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,11 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
 
+import ReactQueryProvider from "@/utils/providers/ReactQueryProvider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 import type { Metadata } from "next";
-import ReactQueryProvider from "@/utils/providers/ReactQueryProvider";
-
 const nunito = Nunito({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
@@ -28,6 +28,7 @@ export default function RootLayout({
           <body className={nunito.className}>
             <NextTopLoader color="#000" />
             {children}
+            <Analytics />
           </body>
         </html>
       </ReactQueryProvider>
