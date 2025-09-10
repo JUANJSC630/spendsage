81e75cf12f17365af382ac01bb0cb0fb9f167314
Juan Saldarriaga <juan@ocmiwc.com>
2025-09-10 19:01:23 -0400

    refactor: update layout structure in ListPaymentSchedulePage for improved responsiveness

--- app/(routes)/list-payment-schedule/page.tsx
+++ app/(routes)/list-payment-schedule/page.tsx
@@ -27,9 +27,9 @@ export default async function ListPaymentSchedulePage() {
       <YearProvider data={data}>
         {/* Header con filtro y botón */}
         <div className="bg-background py-4 px-6 mb-6">
-          <div className="container mx-auto flex flex-row items-center justify-between gap-2 md:gap-8">
+          <div className="container mx-auto flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 sm:gap-2 md:gap-8">
             <h1 className="text-2xl font-bold">Listas de Pagos</h1>
-            <div className="flex items-center gap-4">
+            <div className="flex flex-wrap w-full sm:w-auto items-center gap-2 sm:gap-4">
               <FilterYear data={data} />
               <ButtonAddListPaymentSchedule />
             </div>
